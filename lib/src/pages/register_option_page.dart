8c75230ffd0f6113e747969c8ca1a793fac11ae3
Diego <diegohd-20@outlook.com>
2021-06-20 17:33:50 -0700

    Se integro el registro con firebase

--- lib/src/pages/register_option_page.dart
+++ lib/src/pages/register_option_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food/src/pages/register_page.dart';
 
 class RegisterOpccionPage extends StatefulWidget {
   @override
@@ -9,9 +10,53 @@ class _RegisterOpccionPageState extends State<RegisterOpccionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('asdfadfads'),
+      appBar: AppBar(),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: <Widget>[
+          Icon(Icons.home),
+          _faceBookLRegister(),
+          Divider(),
+          _googleLogin(),
+          Divider(),
+          Center(child: Text('Ó')),
+          Divider(),
+          _crearCuenta(),
+          Divider(),
+        ],
       ),
+    );
+  }
+
+  Widget _crearCuenta() {
+    return FloatingActionButton.extended(
+      heroTag: 'boton crear cuenta',
+      label: const Text('Crear cuenta'),
+      backgroundColor: Colors.pink[100],
+      onPressed: () {
+        final route = MaterialPageRoute(
+          builder: (context) => RegisterPage(),
+        );
+        Navigator.push(context, route);
+      },
+    );
+  }
+
+  Widget _faceBookLRegister() {
+    return ElevatedButton(
+      child: Text(''),
+      onPressed: () {
+        print('object');
+      },
+    );
+  }
+
+  _googleLogin() {
+    return ElevatedButton(
+      child: Text(''),
+      onPressed: () {
+        print('object');
+      },
     );
   }
 }
